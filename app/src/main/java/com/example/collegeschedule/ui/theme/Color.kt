46fbc9c8d5b7738c4648f3596928def19aed905d
opuR1 <lel070320@gmail.com>
2026-01-15 13:31:04 +0700

    Выполнен редизайн интерфейса под Material Design 3

--- app/src/main/java/com/example/collegeschedule/ui/theme/Color.kt
+++ app/src/main/java/com/example/collegeschedule/ui/theme/Color.kt
@@ -2,10 +2,18 @@ package com.example.collegeschedule.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+// Синяя палитра Material 3
+val BluePrimary = Color(0xFF005FB0)
+val BlueOnPrimary = Color(0xFFFFFFFF)
+val BluePrimaryContainer = Color(0xFFD6E3FF)
+val BlueOnPrimaryContainer = Color(0xFF001B3E)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val BlueSecondary = Color(0xFF565E71)
+val BlueOnSecondary = Color(0xFFFFFFFF)
+
+val LightBackground = Color(0xFFFDFBFF)
+val LightOnBackground = Color(0xFF1A1C1E)
+val LightSurface = Color(0xFFFDFBFF)
+val LightOnSurface = Color(0xFF1A1C1E)
+val LightOutline = Color(0xFF74777F)
+val LightOutlineVariant = Color(0xFFC4C6D0)
